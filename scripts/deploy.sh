5fdc7802953d02f5146a5d3abf9593260cce039e
Kevin LeClair <kevin.l@ecobee.com>
2018-05-22 17:18:42 -0400

    Adding in better deployment script

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+PROJECT='baseball-graphql'
+ALIAS='baseball-graphql.now.sh'
+export PATH="./node_modules/.bin:$PATH"
+
+# 1. Wair for deployment ready
+echo "Deploying to now..."
+URL=$(now --token=$NOW_TOKEN --public)
+
+echo "Waiting for URL to be public..."
+await-url $URL
+
+now ls --token $NOW_TOKEN
+
+# 2. Alias
+echo "Setting up now alias..."
+now alias set $URL $ALIAS --token $NOW_TOKEN
+
+# 3. Purge old services
+echo "Purging old services..."
+now remove --yes --safe --token $NOW_TOKEN $PROJECT
+
+# 4. Scale to 1
+echo "Scaling to 1 instance..."
+now scale $ALIAS 1 --token $NOW_TOKEN
+
+# 5. Log results
+echo "Deployment successful!"
+now ls --token $NOW_TOKEN
+now alias ls --token $NOW_TOKEN
